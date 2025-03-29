a33413fca958584d6cef2275ed77e7d1d45c356e
Giuseppe Scarpi <giuseppe.scarpi@tiscali.it>
2025-03-29 22:06:56 +0100

    [ADD] Added new parameters to bypass the caches and change various hyperparameters

--- gaussianiser.py
+++ gaussianiser.py
@@ -1,10 +1,20 @@
 import os
 import random
-import json
 import time
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+
+from preprocessing import load_reviews, preprocess_and_extract_topics
+from embeddings import (
+    EmbeddingsCacheInit,
+    clustering_topics,
+    process_topic_extraction
+)
+from sentiments import (
+    sentimentCacheInit,
+    sentimentCache_getSentiment_and_AdjustedRating
+)
 
 def main():
     ver = "0.3.0"
@@ -16,18 +26,17 @@ def main():
     parser.add_argument("-v", "--version", action="version", version=f"{ver}")
     parser.add_argument("-r", "--runs", type=int, help="Number of runs (default 1)", default=1)
     parser.add_argument("-n", "--noimages", action="store_true", help="Do not show images, useful for batch processing")
+    parser.add_argument("-hc", "--hcluster", type=int, help="Minimum cluster size for HDBSCAN (default 3)", default=3)
+    parser.add_argument("-hs", "--hsample", type=int, help="Minimum samples for HDBSCAN (default 2)", default=2)
+    parser.add_argument("-t", "--threshold", type=float, help="Relevance threshold for clustering (default 0.25)", default=0.25)
+    parser.add_argument("-b", "--bypass", action="store_true", help="Bypass caches")
+    parser.add_argument("-e", "--earlystop", action="store_true", help="Stop after clusters have been printed")
     args = parser.parse_args()
 
     if not os.path.exists(args.filename):
         print(f"File {args.filename} not found.")
         return
 
-    from preprocessing import load_reviews, preprocess_and_extract_topics
-    from embeddings import initialise, clustering_topics, process_topic_extraction
-    from sentiments import (
-        sentimentCache_Load,
-        sentimentCache_getSentiment_and_AdjustedRating
-    )
 
     # Data are stored into a directory named after the topic and the seed
     # For example, if seed is 1967 and the topic is "general", the directory will be "general/1967"
@@ -57,11 +66,10 @@ Files are stored in the following structure:
 """
     )
 
-    # Initialize Mistral AI client and set global embeddings cache file path
-    initialise(topicGeneral, embeddings_cache_file)
-
-    # Load sentiment cache
-    sentimentCache_Load(sentiments_cache_file)
+    # Initialize embeddings cache
+    EmbeddingsCacheInit(embeddings_cache_file, args.bypass)
+    # Initialize sentiment cache
+    sentimentCacheInit(sentiments_cache_file, args.bypass)
 
     label_text = "text"
     label_rating = "rating"
@@ -82,7 +90,12 @@ Files are stored in the following structure:
 
         # Cluster topics based on embeddings calculated during pre-processing
         most_important_clusters: list[tuple] = []
-        most_important_clusters = clustering_topics(preprocessed_reviews)
+        most_important_clusters = clustering_topics(
+            reviews=preprocessed_reviews,
+            relevance_threshold=args.threshold,
+            cluster_size=args.hcluster,
+            min_samples=args.hsample,
+        )
 
         print("\nMost important clusters:")
         print(f"{'Label':<15}{'Centroid':<20}{'Frequency':<15}{'N. aspects':<12}{'Keywords'}")
@@ -91,68 +104,69 @@ Files are stored in the following structure:
             print(f"{label:<15}{cluster_info['centroid']:<20}{cluster_info['frequency']:<15}"
                 f"{cluster_info['n_aspects']:<12}{cluster_info['sample_words']}")
 
-        # Process each review for sentiment update
-        print("Updating sentiments (. = calculated, C = cached, E = error, J Json error)\n")
-        topicsAndDetails: list[tuple[str, float, list[str]]] = []
-        for review in original_reviews:
-            topics = [topic for _, cluster_info in most_important_clusters
-                    for topic in cluster_info["sample_words"].split(", ")
-                    if topic.lower() in review["text"].lower()]
-            topicsAndDetails.append((review["text"], review["overall"], topics))
+        if (not args.earlystop):
+            # Process each review for sentiment update
+            print("Updating sentiments (. = calculated, C = cached, E = error, J Json error)\n")
+            topicsAndDetails: list[tuple[str, float, list[str]]] = []
+            for review in original_reviews:
+                topics = [topic for _, cluster_info in most_important_clusters
+                        for topic in cluster_info["sample_words"].split(", ")
+                        if topic.lower() in review["text"].lower()]
+                topicsAndDetails.append((review["text"], review["overall"], topics))
 
-        calculatedSentiments: list[tuple[str, float]] = []
-        from concurrent.futures import ThreadPoolExecutor
-        with ThreadPoolExecutor(max_workers=6) as executor:
-            calculatedSentiments = list(executor.map(lambda t: sentimentCache_getSentiment_and_AdjustedRating(t[0], t[1], t[2]), topicsAndDetails))
+            calculatedSentiments: list[tuple[str, float]] = []
+            from concurrent.futures import ThreadPoolExecutor
+            with ThreadPoolExecutor(max_workers=6) as executor:
+                calculatedSentiments = list(executor.map(lambda t: sentimentCache_getSentiment_and_AdjustedRating(t[0], t[1], t[2]), topicsAndDetails))
 
-        original_ratings: np.ndarray = np.array([t[1] for t in topicsAndDetails])
-        adjusted_ratings: np.ndarray = np.array([result[1] for result in calculatedSentiments])
-        # This operation allows to change the strength of the adjustment
-        adjustements: np.ndarray = (adjusted_ratings - original_ratings)
-        adjusted_ratings = original_ratings + adjustements * 0.5
-        print("\nDone.")
+            original_ratings: np.ndarray = np.array([t[1] for t in topicsAndDetails])
+            adjusted_ratings: np.ndarray = np.array([result[1] for result in calculatedSentiments])
+            # This operation allows to change the strength of the adjustment
+            adjustements: np.ndarray = (adjusted_ratings - original_ratings)
+            adjusted_ratings = original_ratings + adjustements * 0.5
+            print("\nDone.")
 
-        # Store results in a json file
-        if (not os.path.exists(result_file)):
-            with open(result_file, "wt", encoding="utf-8") as f:
-                f.write("timestamp,run,original,adjusted,seed,reviewID,sentence,appVersion\n")
+            # Store results in a json file
+            if (not os.path.exists(result_file)):
+                with open(result_file, "wt", encoding="utf-8") as f:
+                    f.write("timestamp,run,original,adjusted,seed,reviewID,sentence,appVersion\n")
+                    f.close()
+            with open(result_file, "at", encoding="utf-8") as f:
+                for idx in range(len(calculatedSentiments)):
+                    f.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')},{run+1},"
+                            f"{original_ratings[idx]},{adjusted_ratings[idx]},"
+                            f"{seed},{original_indices.pop()},"
+                            f"\"{topicsAndDetails[idx][0][:48].replace('\n','')}...\",{ver}\n")
                 f.close()
-        with open(result_file, "at", encoding="utf-8") as f:
-            for idx in range(len(calculatedSentiments)):
-                f.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')},{run+1},"
-                        f"{original_ratings[idx]},{adjusted_ratings[idx]},"
-                        f"{seed},{original_indices.pop()},"
-                        f"\"{topicsAndDetails[idx][0][:48].replace('\n','')}...\",{ver}\n")
-            f.close()
 
-        # If args.noimages is not set, plot the results
-        if (not args.noimages):
+            # If args.noimages is not set, plot the results
+            if (not args.noimages):
 
-            # Aggregate and plot results
+                # Aggregate and plot results
 
-            score_range = np.arange(-12.0, 12.0, 0.5)
-            adjusted_counts = {round(r,1): 0 for r in np.round(score_range,1)}
-            for rating in adjusted_ratings:
-                rounded_rating = round(rating*2)/2
-                if rounded_rating in adjusted_counts:
-                    adjusted_counts[rounded_rating] += 1
+                score_range = np.arange(-12.0, 12.0, 0.5)
+                adjusted_counts = {round(r,1): 0 for r in np.round(score_range,1)}
+                for rating in adjusted_ratings:
+                    rounded_rating = round(rating*2)/2
+                    if rounded_rating in adjusted_counts:
+                        adjusted_counts[rounded_rating] += 1
 
-            plt.figure(figsize=(16, 9))
-            plt.subplot(2, 1, 1)
-            plt.hist(original_ratings, bins=5, color="blue", alpha=0.7, edgecolor="black")
-            plt.xlabel("Original Rating")
-            plt.ylabel("Count")
-            plt.title("Distribution of Original Ratings")
-            plt.grid(axis="y", linestyle="--", alpha=0.7)
+                plt.figure(figsize=(16, 9))
+                plt.subplot(2, 1, 1)
+                plt.hist(original_ratings, bins=5, color="blue", alpha=0.7, edgecolor="black")
+                plt.xlabel("Original Rating")
+                plt.ylabel("Count")
+                plt.title("Distribution of Original Ratings")
+                plt.grid(axis="y", linestyle="--", alpha=0.7)
 
-            plt.subplot(2, 1, 2)
-            plt.bar(list(adjusted_counts.keys()), list(adjusted_counts.values()),
-                    color="green", alpha=0.7, edgecolor="black", width=0.1)
-            plt.xlabel("Adjusted Rating")
-            plt.ylabel("Count")
-            plt.title("Distribution of Adjusted Ratings")
-            plt.grid(axis="both", linestyle="--", alpha=0.7)
-            plt.show()
+                plt.subplot(2, 1, 2)
+                plt.bar(list(adjusted_counts.keys()), list(adjusted_counts.values()),
+                        color="green", alpha=0.7, edgecolor="black", width=0.1)
+                plt.xlabel("Adjusted Rating")
+                plt.ylabel("Count")
+                plt.title("Distribution of Adjusted Ratings")
+                plt.grid(axis="both", linestyle="--", alpha=0.7)
+                plt.show()
 
 if __name__ == "__main__":
     main()
